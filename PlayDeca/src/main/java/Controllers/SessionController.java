0ee00a59c39c8868bd0102c5804b901bffe442d1
monsterwhat <monster_what@hotmail.com>
2023-07-03 23:32:57 -0600

    Added UI for Password Change and Email Change
    UI for password and Email change is still missing Logic
    Centered Text for Create Post, Change Password and Change Email

--- PlayDeca/src/main/java/Controllers/SessionController.java
+++ PlayDeca/src/main/java/Controllers/SessionController.java
@@ -23,6 +23,13 @@ public class SessionController implements Serializable{
     
     private String username;
     private String password;
+    private String oldPassword;
+    private String newPassword;
+    private String confirmPassword;
+    private String newEmail;
+    private String AuthCode;
+    
+    private boolean hasPassword;
     
     private Users currentUser;
     
@@ -33,8 +40,9 @@ public class SessionController implements Serializable{
         boolean isValid = UserService.login(username, password);
         if(isValid){
             currentUser = UserService.getSession(username, password);
+            this.hasPassword = true;
             this.username = null;
-            this.password=null;
+            this.password = null;
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Success", "Login successful!"));
             return "index.xhtml?faces-redirect=true";
         }else{
@@ -44,12 +52,17 @@ public class SessionController implements Serializable{
     
     public String logOut(){
         this.currentUser = null;
+        this.hasPassword=false;
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Success", "Logout successful!"));
         return "index.xhtml?faces-redirect=true";
     }
     
     public boolean isValid(){
         return currentUser != null;
+    }
+    
+    public void updateEmail(){
+        
     }
     
     public boolean isAdmin(){
@@ -91,5 +104,55 @@ public class SessionController implements Serializable{
     public void setUserService(UserService UserService) {
         this.UserService = UserService;
     }
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getNewEmail() {
+        return newEmail;
+    }
+
+    public void setNewEmail(String newEmail) {
+        this.newEmail = newEmail;
+    }
+
+    public String getAuthCode() {
+        return AuthCode;
+    }
+
+    public void setAuthCode(String AuthCode) {
+        this.AuthCode = AuthCode;
+    }
+
+    public boolean isHasPassword() {
+        return hasPassword;
+    }
+
+    public void setHasPassword(boolean hasPassword) {
+        this.hasPassword = hasPassword;
+    }
+    
+    
 
 }
